2d28f86d454bf40486297e0b5fe34d74c08c9d37
ifigeneiia <52704264+ifigeneiia@users.noreply.github.com>
2019-12-09 17:37:09 +0200

    Icon Edit Button in Mobile View

--- src/components/UI/EditButton.js
+++ src/components/UI/EditButton.js
@@ -1,10 +1,17 @@
 import React from "react";
+import 'font-awesome/css/font-awesome.min.css';
 
 class EditButton extends React.Component {
     
     render() {
         return (
-            <button className="editButton" onClick={() => this.props.handleEdit && this.props.handleEdit()}>Edit Item</button>
+
+            <div>
+            <button className="editButton" onClick={() => this.props.handleEdit && this.props.handleEdit()}>
+                <span className="editButtonText">Edit Item</span>
+                <i className="fa fa-edit editButtonIcon"></i>
+            </button>
+        </div>
         );
     }
 }

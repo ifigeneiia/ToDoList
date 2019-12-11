8ba29a594a15ddc300b2e7de41045372c8a45440
ifigeneiia <52704264+ifigeneiia@users.noreply.github.com>
2019-12-11 12:32:11 +0200

    Icon Submit Button in Mobile View

--- src/components/UI/SubmitButton.js
+++ src/components/UI/SubmitButton.js
@@ -1,12 +1,17 @@
 import React from "react";
+import 'font-awesome/css/font-awesome.min.css';
 
-class EditButton extends React.Component {
+
+class SubmitButton extends React.Component {
     
     render() {
         return (
-            <button className="submitButton" onClick={() => this.props.handleSubmit && this.props.handleSubmit()}>Submit</button>
+            <button className="submitButton " onClick={() => this.props.handleSubmit && this.props.handleSubmit()}>
+                <span className="submitButtonText">Submit</span>
+                 <i className="fa fa-check-square-o submitButtonIcon"></i>
+            </button>
         );
     }
 }
 
-export default EditButton;
+export default SubmitButton;
